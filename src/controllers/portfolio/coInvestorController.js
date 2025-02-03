83c37c8673ee383ecb9dc1e16dceb6dcba41cc39
nayan-kunwar26 <nayan@pearlorganisation.com>
2025-02-03 10:13:00 +0530

    feat: add update and delete functionality for co-investor records; implement pagination for retrieval

--- src/controllers/portfolio/coInvestorController.js
+++ src/controllers/portfolio/coInvestorController.js
@@ -1,7 +1,11 @@
 import CoInvestor from "../../models/portfolio/coInvestor.js";
-import { uploadFileToCloudinary } from "../../utils/cloudinaryConfig.js";
+import {
+  deleteFileFromCloudinary,
+  uploadFileToCloudinary,
+} from "../../utils/cloudinaryConfig.js";
 import ApiErrorResponse from "../../utils/errors/ApiErrorResponse.js";
 import { asyncHandler } from "../../utils/errors/asyncHandler.js";
+import { paginate } from "../../utils/pagination.js";
 
 export const createCoInvestor = asyncHandler(async (req, res, next) => {
   const { logo } = req.files;
@@ -24,7 +28,13 @@ export const createCoInvestor = asyncHandler(async (req, res, next) => {
 });
 
 export const getAllCoInvestor = asyncHandler(async (req, res, next) => {
-  const coInvestorInfo = await CoInvestor.find();
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+  const { data: coInvestorInfo, pagination } = await paginate(
+    CoInvestor,
+    page,
+    limit
+  );
   if (!coInvestorInfo) {
     return next(new ApiErrorResponse("Co-Investor not found", 404));
   }
@@ -32,6 +42,59 @@ export const getAllCoInvestor = asyncHandler(async (req, res, next) => {
   return res.status(200).json({
     success: true,
     message: "Co-Investor retrieved successfully",
+    pagination,
     data: coInvestorInfo,
+  });
+});
+
+export const updateCoInvestorById = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+
+  const coInvestor = await CoInvestor.findById(id);
+  if (!coInvestor) {
+    return next(new ApiErrorResponse("Co-Investor not found", 404));
+  }
+  const { logo } = req.files;
+  const uploadedLogo = logo ? await uploadFileToCloudinary(logo) : undefined;
+  if (uploadedLogo) {
+    if (coInvestor.logo) {
+      await deleteFileFromCloudinary(coInvestor.logo);
+    }
+  }
+  const updatedCoInvestor = await CoInvestor.findByIdAndUpdate(
+    req.params.id,
+    {
+      ...req.body,
+      logo: uploadedLogo ? uploadedLogo[0] : undefined,
+    },
+    { new: true, runValidators: true }
+  ).lean();
+
+  if (!updatedCoInvestor) {
+    return next(new ApiErrorResponse("Co-Investor not found", 404));
+  }
+
+  return res.status(200).json({
+    success: true,
+    message: "Co-Investor updated successfully",
+    data: updatedCoInvestor,
+  });
+});
+
+export const deleteCoInvestorById = asyncHandler(async (req, res, next) => {
+  const deletedCoInvestor = await CoInvestor.findByIdAndDelete(
+    req.params.id
+  ).lean();
+
+  if (!deletedCoInvestor) {
+    return next(new ApiErrorResponse("Co-Investor not found", 404));
+  }
+
+  if (deletedCoInvestor.logo) {
+    await deleteFileFromCloudinary(deletedCoInvestor.logo);
+  }
+  return res.status(200).json({
+    success: true,
+    message: "Co-Investor deleted successfully",
   });
 });
--- src/routes/portfolio/coInvestorRoute.js
+++ src/routes/portfolio/coInvestorRoute.js
@@ -1,12 +1,18 @@
 import express from "express";
 import {
   createCoInvestor,
+  deleteCoInvestorById,
   getAllCoInvestor,
+  updateCoInvestorById,
 } from "../../controllers/portfolio/coInvestorController.js";
 import fileParser from "../../middlewares/fileParser.js";
 
 const router = express.Router();
 
 router.route("/").post(fileParser, createCoInvestor).get(getAllCoInvestor);
+router
+  .route("/:id")
+  .patch(fileParser, updateCoInvestorById)
+  .delete(deleteCoInvestorById);
 
 export default router;
