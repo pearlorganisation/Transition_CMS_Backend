bec6fc428606dd0aeee0f7ed838b3ac6375502cc
nayan-kunwar26 <nayan@pearlorganisation.com>
2025-02-04 10:45:58 +0530

    feat: implement CRUD operations for investment timelines; add pagination and image handling

--- src/controllers/portfolio/investmentTimelineController.js
+++ src/controllers/portfolio/investmentTimelineController.js
@@ -1,7 +1,11 @@
 import InvestmentTimeline from "../../models/portfolio/investmentTimeline.js";
-import { uploadFileToCloudinary } from "../../utils/cloudinaryConfig.js";
+import {
+  deleteFileFromCloudinary,
+  uploadFileToCloudinary,
+} from "../../utils/cloudinaryConfig.js";
 import ApiErrorResponse from "../../utils/errors/ApiErrorResponse.js";
 import { asyncHandler } from "../../utils/errors/asyncHandler.js";
+import { paginate } from "../../utils/pagination.js";
 
 export const createInvestmentTimeline = asyncHandler(async (req, res) => {
   const { image } = req.files;
@@ -12,7 +16,7 @@ export const createInvestmentTimeline = asyncHandler(async (req, res) => {
     ...req.body,
     image: uploadedImage ? uploadedImage[0] : null,
   });
-  console.log("sdf", newInvestmentTimeline);
+
   if (!newInvestmentTimeline) {
     return next(
       new ApiErrorResponse("Investment Timeline creation failed", 400)
@@ -25,3 +29,101 @@ export const createInvestmentTimeline = asyncHandler(async (req, res) => {
     data: newInvestmentTimeline,
   });
 });
+
+export const getInvestmentTimelineById = asyncHandler(
+  async (req, res, next) => {
+    const { id } = req.params;
+    const investmentTimeline = await InvestmentTimeline.findById(id).populate(
+      "cards"
+    );
+    if (!investmentTimeline) {
+      return next(new ApiErrorResponse("Investment Timeline not found", 404));
+    }
+    return res.status(200).json({
+      success: true,
+      message: "Investment Timeline retrieved successfully",
+      data: investmentTimeline,
+    });
+  }
+);
+
+export const getAllInvestmentTimelines = asyncHandler(
+  async (req, res, next) => {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+
+    const { data: investmentTimelines, pagination } = await paginate(
+      InvestmentTimeline,
+      page,
+      limit,
+      [{ path: "cards" }]
+    );
+
+    if (!investmentTimelines) {
+      return next(new ApiErrorResponse("Investment Timelines not found", 404));
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Investment Timelines retrieved successfully",
+      pagination,
+      data: investmentTimelines,
+    });
+  }
+);
+
+export const updateInvestmentTimelineById = asyncHandler(
+  async (req, res, next) => {
+    const { id } = req.params;
+    const investmentTimeline = await InvestmentTimeline.findById(id);
+    if (!investmentTimeline) {
+      return next(new ApiErrorResponse("Investment Timeline not found", 404));
+    }
+
+    const { image } = req.files;
+    const uploadedImage = image
+      ? await uploadFileToCloudinary(image)
+      : undefined;
+
+    if (uploadedImage && investmentTimeline.image) {
+      await deleteFileFromCloudinary(investmentTimeline.image);
+    }
+
+    const updatedInvestmentTimeline =
+      await InvestmentTimeline.findByIdAndUpdate(
+        id,
+        {
+          ...req.body,
+          image: uploadedImage ? uploadedImage[0] : undefined,
+        },
+        { new: true, runValidators: true }
+      ).populate("cards");
+
+    if (!updatedInvestmentTimeline) {
+      return next(new ApiErrorResponse("Investment Timeline not found", 404));
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Investment Timeline updated successfully",
+      data: updatedInvestmentTimeline,
+    });
+  }
+);
+
+export const deleteInvestmentTimelineById = asyncHandler(
+  async (req, res, next) => {
+    const { id } = req.params;
+    const investmentTimeline = await InvestmentTimeline.findByIdAndDelete(id);
+    if (!investmentTimeline) {
+      return next(new ApiErrorResponse("Investment Timeline not found", 404));
+    }
+    if (investmentTimeline.image) {
+      await deleteFileFromCloudinary(investmentTimeline.image);
+    }
+    return res.status(200).json({
+      success: true,
+      message: "Investment Timeline deleted successfully",
+    });
+  }
+);
--- src/routes/portfolio/investmentTimelineRoute.js
+++ src/routes/portfolio/investmentTimelineRoute.js
@@ -1,9 +1,24 @@
 import express from "express";
 import fileParser from "../../middlewares/fileParser.js";
-import { createInvestmentTimeline } from "../../controllers/portfolio/investmentTimelineController.js";
+import {
+  createInvestmentTimeline,
+  deleteInvestmentTimelineById,
+  getAllInvestmentTimelines,
+  getInvestmentTimelineById,
+  updateInvestmentTimelineById,
+} from "../../controllers/portfolio/investmentTimelineController.js";
 
 const router = express.Router();
 
-router.route("/").post(fileParser, createInvestmentTimeline);
+router
+  .route("/")
+  .post(fileParser, createInvestmentTimeline)
+  .get(getAllInvestmentTimelines);
+
+router
+  .route("/:id")
+  .get(getInvestmentTimelineById)
+  .patch(fileParser, updateInvestmentTimelineById)
+  .delete(deleteInvestmentTimelineById);
 
 export default router;
