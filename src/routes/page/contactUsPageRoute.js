c1418a2d9e1f38d1c2b1e9f19dddac3a8005e7f9
nayan-kunwar26 <nayan@pearlorganisation.com>
2025-02-13 12:35:41 +0530

    refactor: remove unused page controllers and routes; enhance contact functionality with update and delete operations

--- src/routes/page/contactUsPageRoute.js
+++ src/routes/page/contactUsPageRoute.js
@@ -1,10 +1,14 @@
 import express from "express";
- 
-import fileParser from "../../middlewares/fileParser.js";
-import { createContact, getContacts } from "../../controllers/page/contact.js";
+import {
+  createContact,
+  deleteContactById,
+  getContacts,
+  updateContactById,
+} from "../../controllers/page/contact.js";
 
 const router = express.Router();
 
 router.route("/").post(createContact).get(getContacts);
+router.route("/:id").patch(updateContactById).delete(deleteContactById);
 
 export default router;
